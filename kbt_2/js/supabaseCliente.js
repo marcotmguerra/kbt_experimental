d505edc17f452dcdcc0448749b99a85ddf691385
marcotmguerra <mguerra155@gmail.com>
2026-01-29 20:48:24 -0300

    correcao do supabase

--- kbt_2/js/supabaseCliente.js
+++ kbt_2/js/supabaseCliente.js
@@ -1,8 +1,7 @@
 // /public/js/supabaseCliente.js
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const SUPABASE_URL = "https://oqrutzrrgwnllnsqfjxh.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9xcnV0enJyZ3dubGxuc3FmanhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk2NDQ4OTcsImV4cCI6MjA4NTIyMDg5N30.uk7qEVUkU2Kfvuv2txHyIXD2LVPdKAdLv8mG3TBCCf8";
 
-// Corrigido para usar SUPABASE_KEY
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
